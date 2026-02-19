18beaeef7ee304a00d873acd31df5664c2b45ba2
MARCELO MARES RODRIGUES <marcelom2205@hotmail.com>
2026-02-19 20:08:09 -0300

    Desafio 8.24

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/disassembler/OrderPersistenceEntityDisassembler.java
@@ -3,10 +3,13 @@ package com.algaworks.algashop.ordering.infrastructure.persistence.disassembler;
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.entity.OrderStatus;
 import com.algaworks.algashop.ordering.domain.model.entity.PaymentMethod;
-import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
-import com.algaworks.algashop.ordering.domain.model.valueobject.Quantity;
+import com.algaworks.algashop.ordering.domain.model.valueobject.*;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.CustomerId;
 import com.algaworks.algashop.ordering.domain.model.valueobject.id.OrderId;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.RecipientEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
 
@@ -29,6 +32,58 @@ public class OrderPersistenceEntityDisassembler {
                 .readyAt(persistenceEntity.getReadyAt())
                 .items(new HashSet<>())
                 .version(persistenceEntity.getVersion())
+                .billing(toBilling(persistenceEntity.getBilling()))
+                .shipping(toShipping(persistenceEntity.getShipping()))
+                .build();
+    }
+
+    private Billing toBilling(BillingEmbeddable billingEmbeddable) {
+        if (billingEmbeddable == null) {
+            return null;
+        }
+        return Billing.builder()
+                .fullName(new FullName(billingEmbeddable.getFirstName(), billingEmbeddable.getLastName()))
+                .document(new Document(billingEmbeddable.getDocument()))
+                .phone(new Phone(billingEmbeddable.getPhone()))
+                .address(toAddress(billingEmbeddable.getAddress()))
+                .build();
+    }
+
+    private Shipping toShipping(ShippingEmbeddable shippingEmbeddable) {
+        if (shippingEmbeddable == null) {
+            return null;
+        }
+        return Shipping.builder()
+                .cost(new Money(shippingEmbeddable.getCost()))
+                .expectedDate(shippingEmbeddable.getExpectedDate())
+                .address(toAddress(shippingEmbeddable.getAddress()))
+                .recipient(toRecipient(shippingEmbeddable.getRecipient()))
+                .build();
+    }
+
+    private Address toAddress(AddressEmbeddable addressEmbeddable) {
+        if (addressEmbeddable == null) {
+            return null;
+        }
+        return Address.builder()
+                .street(addressEmbeddable.getStreet())
+                .number(addressEmbeddable.getNumber())
+                .complement(addressEmbeddable.getComplement())
+                .neighborhood(addressEmbeddable.getNeighborhood())
+                .city(addressEmbeddable.getCity())
+                .state(addressEmbeddable.getState())
+                .zipCode(new ZipCode(addressEmbeddable.getZipCode()))
+                .build();
+    }
+
+    private Recipient toRecipient(RecipientEmbeddable recipientEmbeddable) {
+        if (recipientEmbeddable == null) {
+            return null;
+        }
+        return Recipient.builder()
+                .fullName(new FullName(recipientEmbeddable.getFirstName(), recipientEmbeddable.getLastName()))
+                .document(new Document(recipientEmbeddable.getDocument()))
+                .phone(new Phone(recipientEmbeddable.getPhone()))
                 .build();
     }
 
