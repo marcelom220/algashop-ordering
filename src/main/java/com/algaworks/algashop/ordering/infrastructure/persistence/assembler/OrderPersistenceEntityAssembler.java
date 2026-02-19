f9453bdca986d4cc28f0797da22884a931909ffd
MARCELO MARES RODRIGUES <marcelom2205@hotmail.com>
2026-02-19 19:52:38 -0300

    Desafio 8.24

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
@@ -1,6 +1,14 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.assembler;
 
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Address;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Billing;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Recipient;
+import com.algaworks.algashop.ordering.domain.model.valueobject.Shipping;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.RecipientEmbeddable;
+import com.algaworks.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
 
@@ -23,7 +31,56 @@ public class OrderPersistenceEntityAssembler {
         orderPersistenceEntity.setCanceledAt(order.canceledAt());
         orderPersistenceEntity.setReadyAt(order.readyAt());
         orderPersistenceEntity.setVersion(order.version());
+
+        if (order.billing() != null) {
+            orderPersistenceEntity.setBilling(toBillingEmbeddable(order.billing()));
+        }
+
+        if (order.shipping() != null) {
+            orderPersistenceEntity.setShipping(toShippingEmbeddable(order.shipping()));
+        }
+
         return orderPersistenceEntity;
+    }
+
+    private BillingEmbeddable toBillingEmbeddable(Billing billing) {
+        return BillingEmbeddable.builder()
+                .firstName(billing.fullName().firstName())
+                .lastName(billing.fullName().lastName())
+                .document(billing.document().value())
+                .phone(billing.phone().value())
+                .address(toAddressEmbeddable(billing.address()))
+                .build();
+    }
+
+    private ShippingEmbeddable toShippingEmbeddable(Shipping shipping) {
+        return ShippingEmbeddable.builder()
+                .cost(shipping.cost().value())
+                .expectedDate(shipping.expectedDate())
+                .address(toAddressEmbeddable(shipping.address()))
+                .recipient(toRecipientEmbeddable(shipping.recipient()))
+                .build();
+    }
+
+    private AddressEmbeddable toAddressEmbeddable(Address address) {
+        return AddressEmbeddable.builder()
+                .street(address.street())
+                .number(address.number())
+                .complement(address.complement())
+                .neighborhood(address.neighborhood())
+                .city(address.city())
+                .state(address.state())
+                .zipCode(address.zipCode().value())
+                .build();
+    }
+
+    private RecipientEmbeddable toRecipientEmbeddable(Recipient recipient) {
+        return RecipientEmbeddable.builder()
+                .firstName(recipient.fullName().firstName())
+                .lastName(recipient.fullName().lastName())
+                .document(recipient.document().value())
+                .phone(recipient.phone().value())
+                .build();
     }
 
 }
